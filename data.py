58d3577994c81b91d59af3fcac189a2899f5ba8a
SHIV RAJ SINGH <144125626+imuniqueshiv@users.noreply.github.com>
2024-12-13 20:25:23 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,34 @@
+question_data = [
+
+    {"type": "boolean", "difficulty": "medium", "category": "Science: Computers",
+     "question": "MacOS is based on Linux.", "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"type": "boolean", "difficulty": "medium", "category": "Science: Computers",
+     "question": "All program codes have to be compiled into an executable file in order to be run.\nThis file can then be executed on any machine.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"type": "boolean", "difficulty": "medium", "category": "Science: Computers",
+     "question": "The first computer bug was formed by faulty wires.", "correct_answer": "False",
+     "incorrect_answers": ["True"]},
+    {"type": "boolean", "difficulty": "medium", "category": "Science: Computers",
+     "question": "&quot;Windows NT&quot; is a monolithic kernel.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"type": "boolean", "difficulty": "medium", "category": "Science: Computers",
+     "question": "The last Windows operating system to be based on the Windows 9x kernel was Windows 98.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"type": "boolean", "difficulty": "medium", "category": "Science: Computers",
+     "question": "The HTML5 standard was published in 2014.", "correct_answer": "True",
+     "incorrect_answers": ["False"]},
+    {"type": "boolean", "difficulty": "medium", "category": "Science: Computers",
+     "question": "Linus Sebastian is the creator of the Linux kernel, "
+                 "which went on to be used in Linux, Android, and Chrome OS.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"type": "boolean", "difficulty": "medium", "category": "Science: Computers",
+     "question": "The common software-programming acronym &quot;I18N&quot;\ncomes from the term &quot;Interlocalization&quot;.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"type": "boolean", "difficulty": "medium", "category": "Science: Computers",
+     "question": "Early RAM was directly seated onto the motherboard and could not be easily removed.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"type": "boolean", "difficulty": "medium", "category": "Science: Computers",
+     "question": "To bypass US Munitions Export Laws, "
+                 "the creator of the PGP published all the source code in book form. ",
+     "correct_answer": "True", "incorrect_answers": ["False"]}
+]
